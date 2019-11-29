4db3cbd0af2ee4eb46867702cd0190da1980aedb
Mohamed Mayallo <reckoning58@gmail.com>
2019-11-29 22:45:02 +0200

    Set database relationships

--- app/Reply.php
+++ app/Reply.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reply extends Model
 {
-    //
+    public function question() {
+      return $this->belongsTo(Question::class);
+    }
+
+    public function replies() {
+      return $this->hasMany(Reply::class);
+    }
 }
