7023d506a33f472f465eb82ee19e66775abadc65
Mohamed Mayallo <reckoning58@gmail.com>
2019-12-07 00:57:56 +0200

    Adding Vue && Vuetify

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <toolbar></toolbar>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import toolbar from "./Toolbar";
+import footer from "./Footer";
+
+export default {
+  components: {
+    toolbar,
+    "app-footer": footer
+  }
+};
+</script>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>Document</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+  <div id="app">
+    <v-app>
+      <home></home>
+    </v-app>
+  </div>
+  <script src="{{asset('js/app.js')}}"></script>
+</body>
+</html>
